9b902fcaa0013fe76f6f0e11f31b2aa3947dd409
amir <amir.abdullaev@email.cz>
2024-11-09 23:40:12 +0100

    seting up end2end testing

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,4 +1,5 @@
-import { View, Text, TouchableOpacity, ScrollView, SafeAreaView } from 'react-native';
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import Header from '../../components/Header';
 
 export default function Create() {
