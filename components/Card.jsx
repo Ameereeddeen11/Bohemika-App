1e308767943e370cdd0782d7e6255031d2a1f98d
amir <amir.abdullaev@email.cz>
2024-11-10 13:28:07 +0100

    improve design of Card component

--- components/Card.jsx
+++ components/Card.jsx
@@ -5,31 +5,33 @@ import Icon2 from 'react-native-vector-icons/Entypo';
 
 export default function Card(props) {
     return (
-        <View className="flex-row p-4 bg-white rounded-lg shadow-md h-60 my-2">
+        <View className="flex-row p-4 bg-white rounded-lg shadow-lg h-60 my-4 space-x-4">
             <View className="justify-center">
                 <Image
                     source={require('../assets/images/logo2.png')}
-                    className="w-20 h-20"
+                    className="w-24 h-24 rounded-lg"
                 />
             </View>
 
-            <View className="flex-1 px-4 justify-center">
-                <Text className="text-xl font-bold">{props.nazev}</Text>
-                <Text className="text-sm text-gray-600">{props.number} This is the mini description of the card content, giving a brief overview. {"\n"}</Text>
-                <View className="flex-row justify-between p-4 bg-white rounded-lg">
-                    <View className="justify-center">
-                        <Text className="text-gray-500 font-semibold">
-                            <Icon2 name="calendar" size={15}/> {props.platnostOd}
+            <View className="flex-1 px-2 justify-center">
+                <Text className="text-lg font-bold text-gray-800">{props.nazev}</Text>
+                <Text className="text-sm text-gray-500 mt-1">
+                    {props.number} This is the mini description of the card content, giving a brief overview.
+                </Text>
+
+                <View className="flex-row justify-between items-center mt-4">
+                    <View>
+                        <Text className="text-gray-500 font-medium">
+                            <Icon2 name="calendar" size={15} /> {props.platnostOd}
                         </Text>
-                        <Text className="text-gray-500 font-semibold py-1">
+                        <Text className="text-gray-500 font-medium mt-1">
                             <Icon name="timer-sand" size={15} /> {props.platnostDo}
                         </Text>
                     </View>
-                    <View className="flex-col justify-center items-end">
-                        <TouchableOpacity className="bg-sky-500/100 rounded-xl min-h-[40px] min-w-[60px] justify-center items-center">
-                            <Text className={"text-slate-50 font-psemibold text-base"}><Icon2 name="text-document-inverted" size={20}/></Text>
-                        </TouchableOpacity>
-                    </View>
+
+                    <TouchableOpacity className="bg-blue-500 rounded-lg h-10 w-10 flex justify-center items-center">
+                        <Icon2 name="text-document-inverted" size={20} color="white" />
+                    </TouchableOpacity>
                 </View>
             </View>
         </View>
