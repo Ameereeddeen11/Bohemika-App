caa15dffe54a557556b550637feb592bb93092f6
amir <amir.abdullaev@email.cz>
2024-11-20 22:10:28 +0100

    improving the create page

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,179 +1,140 @@
-import React from 'react';
-import {
-  StyleSheet,
-  ScrollView,
-  Text,
-  TouchableOpacity,
-  View,
-  Image,
-} from 'react-native';
+// import React from 'react';
+// import {
+//   StyleSheet,
+//   ScrollView,
+//   Text,
+//   TouchableOpacity,
+//   View
+// } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import FeatherIcon from 'react-native-vector-icons/Feather';
+import AntDesign from 'react-native-vector-icons/AntDesign';
 import Header from '../../components/Header';
 
-const lessons = [
-  {
-    name: 'Squat',
-    cal: 22,
-    duration: 10,
-  },
-  {
-    name: 'Pull-up',
-    cal: 12,
-    duration: 15,
-  },
-  {
-    name: 'Push-up',
-    cal: 12,
-    duration: 5,
-  },
-  {
-    name: 'Calisthenics',
-    cal: 33,
-    duration: 12,
-  },
-  {
-    name: 'Lunge',
-    cal: 44,
-    duration: 10,
-  },
-  {
-    name: 'Squat',
-    cal: 22,
-    duration: 10,
-  },
-  {
-    name: 'Pull-up',
-    cal: 12,
-    duration: 15,
-  },
-  {
-    name: 'Push-up',
-    cal: 12,
-    duration: 5,
-  },
-  {
-    name: 'Calisthenics',
-    cal: 33,
-    duration: 12,
-  },
-  {
-    name: 'Lunge',
-    cal: 44,
-    duration: 10,
-  },
+// const lessons = [
+//   {
+//     name: 'Squat',
+//     cal: 22,
+//     duration: 10,
+//   },
+//   {
+//     name: 'Pull-up',
+//     cal: 12,
+//     duration: 15,
+//   },
+//   {
+//     name: 'Push-up',
+//     cal: 12,
+//     duration: 5,
+//   },
+//   {
+//     name: 'Calisthenics',
+//     cal: 33,
+//     duration: 12,
+//   }
+// ];
+
+// export default function Create() {
+//   return (
+//     <SafeAreaView className="flex-1 bg-gray-50">
+//       <Header />
+//       <ScrollView
+//         className="p-4"
+//         contentContainerStyle={{ paddingBottom: 20 }}
+//         keyboardShouldPersistTaps="handled"
+//         nestedScrollEnabled
+//       >
+//         {/* Header */}
+//         <View className="flex-row items-center justify-between p-4">
+//           <Text className="text-2xl font-semibold text-gray-800 ">Seznam nahranych</Text>
+//           <TouchableOpacity
+//             className="bg-blue-600 px-4 py-2 rounded-lg shadow-sm hover:bg-blue-700 active:bg-blue-800"
+//             onPress={() => alert('Nahrávat dokument')}
+//           >
+//             <View className="flex-row items-center justify-between">
+//               <AntDesign name='pluscircleo' size={14} color="white" />
+//               <Text className="text-white text-sm font-semibold tracking-wide px-2">
+//                 Nahrát
+//               </Text>
+//             </View>
+//           </TouchableOpacity>
+//         </View>
+
+//         {/* Lessons */}
+//         {lessons.map(({ name, cal, duration }, index) => (
+//           <TouchableOpacity
+//             key={index}
+//             className="bg-white rounded-lg shadow-sm p-4 mb-3 active:bg-gray-100"
+//             onPress={() => {
+//               // handle onPress
+//             }}
+//           >
+//             <View className="flex-row items-center justify-between">
+//               {/* Left content */}
+//               <View>
+//                 <Text className="text-lg font-semibold text-gray-800">{name}</Text>
+//                 <Text className="text-gray-600 text-sm mt-1">
+//                   {duration} mins • {cal} cals
+//                 </Text>
+//               </View>
+//               {/* Right icon */}
+//               <FeatherIcon name="chevron-right" size={24} color="#9ca3af" />
+//             </View>
+//           </TouchableOpacity>
+//         ))}
+//       </ScrollView>
+//     </SafeAreaView>
+//   );
+// }
+
+
+import React from 'react';
+import { View, Text, FlatList, TouchableOpacity, ScrollView } from 'react-native';
+
+const data = [
+  { id: '1', repo: 'Bohemika-App', title: 'home page', time: '1d' },
+  { id: '2', repo: 'recipe', title: 'docker compose is working but logout isn\'t', time: '9d' },
 ];
 
-export default function Example() {
+export default function Create() {
+  
+  const renderItem = ({ item }) => (
+    <TouchableOpacity
+        className='flex-row justify-between items-center p-4 border-b border-gray-200'
+    >
+      <View>
+        <Text className='text-sm font-semibold text-black'>
+          {item.repo}
+        </Text>
+        <Text className='text-lg text-black'>{item.title}</Text>
+      </View>
+      <Text className='text-black text-xs'>{item.time}</Text>
+    </TouchableOpacity>
+  );
+
   return (
-    <SafeAreaView style={{flex: 1}}>
-        <Header/>
-        <ScrollView
-            className="p-4"
-            contentContainerStyle={{ paddingBottom: 20 }}
-            keyboardShouldPersistTaps="handled"
-            nestedScrollEnabled
-        >
-            <View className="flex-row items-center justify-between p-2 bg-gray-100 rounded-lg shadow-sm">
-                <Text className="text-2xl font-semibold text-gray-800">Seznam vytvořených</Text>
-                <TouchableOpacity 
-                    className="bg-blue-600 p-3 rounded-xl h-10 w-28 items-center justify-center shadow-md hover:bg-blue-700 active:bg-blue-800" 
-                    onPress={() => alert('Nahrávat dokument')}
-                >
-                    <Text className="text-white text-sm font-semibold tracking-wide">Nahrát</Text>
-                </TouchableOpacity>
-            </View>
-            {lessons.map(({ name, cal, duration, img }, index) => {
-                return (
-                    <TouchableOpacity
-                    key={index}
-                    onPress={() => {
-                        // handle onPress
-                    }}>
-                        <View style={styles.card}>
-                            <View>
-                                <Text style={styles.cardTitle}>{name}</Text>
-
-                                <View style={styles.cardStats}>
-                                    <View style={styles.cardStatsItem}>
-                                        <FeatherIcon color="#636a73" name="clock" />
-
-                                        <Text style={styles.cardStatsItemText}>
-                                            {duration} mins
-                                        </Text>
-                                    </View>
-
-                                    <View style={styles.cardStatsItem}>
-                                        <FeatherIcon color="#636a73" name="zap" />
-                                        <Text style={styles.cardStatsItemText}>{cal} cals</Text>
-                                    </View>
-                                </View>
-                            </View>
-
-                            <View style={styles.cardAction}>
-                                <FeatherIcon
-                                    color="#9ca3af"
-                                    name="chevron-right"
-                                    size={22} 
-                                />
-                            </View>
-                        </View>
-                    </TouchableOpacity>
-                );
-            })}
-        </ScrollView>
+    <SafeAreaView className="flex-1 bg-gray-50">
+       <Header />
+       <ScrollView
+        className="p-4"
+        contentContainerStyle={{ paddingBottom: 20 }}
+        keyboardShouldPersistTaps="handled"
+        nestedScrollEnabled
+      >
+        <View className='flex-1'>
+          <View className={('flex-row justify-around mb-2 px-4')}>
+            <Text className={('text-black')}>Open</Text>
+            <Text className={('text-black')}>Created by me</Text>
+            <Text className={('text-black')}>Visibility</Text>
+            <Text className={('text-black')}>Organization</Text>
+          </View>
+          <FlatList
+            data={data}
+            keyExtractor={(item) => item.id}
+            renderItem={renderItem}
+          />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 24,
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: '700',
-    color: '#1d1d1d',
-    marginBottom: 12,
-  },
-  /** Card */
-  card: {
-    paddingVertical: 14,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  },
-  cardImg: {
-    width: 50,
-    height: 50,
-    borderRadius: 9999,
-    marginRight: 12,
-  },
-  cardTitle: {
-    fontSize: 15,
-    fontWeight: '600',
-    color: '#000',
-    marginBottom: 8,
-  },
-  cardStats: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  cardStatsItem: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginRight: 8,
-  },
-  cardStatsItemText: {
-    fontSize: 13,
-    fontWeight: '500',
-    color: '#636a73',
-    marginLeft: 2,
-  },
-  cardAction: {
-    marginLeft: 'auto',
-  },
-});
